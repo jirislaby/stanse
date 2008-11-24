f7e4d284465ce2bf49209871f2e92df70d7e1439
xslaby <xslaby@98efdb2a-0913-0410-9d53-f03ad6a1d54d>
2008-11-24 13:17:43 +0000

    FunctionNameTrigger: add more checking
    Function needn't be called directly by name, but with structure
    members. Be aware of that from now on.
    
    git-svn-id: https://arran.fi.muni.cz/svn/stanse/trunk@315 98efdb2a-0913-0410-9d53-f03ad6a1d54d

--- src/cz/muni/stanse/automatonchecker/FunctionNameTrigger.java
+++ src/cz/muni/stanse/automatonchecker/FunctionNameTrigger.java
@@ -4,7 +4,7 @@ package cz.muni.stanse.automatonchecker;
 import cz.muni.stanse.xml2cfg.CFGNode;
 
 import org.dom4j.Element;
-
+import org.dom4j.Node;
 
 /**
  * FunctionNameTrigger is triggered when a function names match
@@ -18,8 +18,16 @@ public class FunctionNameTrigger extends AbstractTrigger {
     
     public boolean isTriggered(CFGNode from, CFGNode to) {
         Element edge = from.getCFG().getEdgeElement(from, to);
-        return edge.getName().equals("functionCall") &&
-                pass.equals(edge.selectSingleNode("./expression/id").getText());
+
+	if (!edge.getName().equals("functionCall"))
+	    return false;
+
+	Node id = edge.selectSingleNode("./expression/id");
+	/* call not by name */
+	if (id == null)
+	    return false;
+
+        return pass.equals(id.getText());
     }
 
     public void loadTrigger(String param) {
