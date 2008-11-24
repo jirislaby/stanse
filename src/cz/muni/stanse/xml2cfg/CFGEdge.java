0fc26e27461b6b105f89d99c96d3c8ee1313c8e8
xslaby <xslaby@98efdb2a-0913-0410-9d53-f03ad6a1d54d>
2008-11-24 13:15:51 +0000

    xml2cfg: whitespace cleanup
    git-svn-id: https://arran.fi.muni.cz/svn/stanse/trunk@310 98efdb2a-0913-0410-9d53-f03ad6a1d54d

--- src/cz/muni/stanse/xml2cfg/CFGEdge.java
+++ src/cz/muni/stanse/xml2cfg/CFGEdge.java
@@ -1,22 +1,20 @@
 package cz.muni.stanse.xml2cfg;
 
-
 public class CFGEdge {
 
     private CFGNode from;
     private CFGNode to;
 
-    
     /**
      * Private -- use getInstace instead.
      * @param from From node (parent)
      * @param to To node (child)
      */
     private CFGEdge(CFGNode from, CFGNode to) {
-        this.from = from;
-        this.to = to;
+	this.from = from;
+	this.to = to;
     }
-    
+
     /**
      * Factory. Cache may be introduced.
      * @param from From node (parent)
@@ -24,52 +22,51 @@ public class CFGEdge {
      * @return Instance of CFGEdge
      */
     public static CFGEdge getInstance(CFGNode from, CFGNode to) {
-        
-        CFGEdge retEdge;
-        retEdge= new CFGEdge(from, to);
-        
-        return retEdge;
+
+	CFGEdge retEdge;
+	retEdge= new CFGEdge(from, to);
+
+	return retEdge;
     }
 
     public CFGNode getFrom() {
-        return from;
+	return from;
     }
 
     public CFGNode getTo() {
-        return to;
+	return to;
     }
-    
+
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final CFGEdge other = (CFGEdge) obj;
-        if (this.from != other.from && (this.from == null || !this.from.equals(other.from))) {
-            return false;
-        }
-        if (this.to != other.to && (this.to == null || !this.to.equals(other.to))) {
-            return false;
-        }
-        return true;
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	final CFGEdge other = (CFGEdge) obj;
+	if (this.from != other.from && (this.from == null || !this.from.equals(other.from))) {
+	    return false;
+	}
+	if (this.to != other.to && (this.to == null || !this.to.equals(other.to))) {
+	    return false;
+	}
+	return true;
     }
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 41 * hash + (this.from != null ? this.from.hashCode() : 0);
-        hash = 41 * hash + (this.to != null ? this.to.hashCode() : 0);
-        return hash;
+	int hash = 7;
+	hash = 41 * hash + (this.from != null ? this.from.hashCode() : 0);
+	hash = 41 * hash + (this.to != null ? this.to.hashCode() : 0);
+	return hash;
     }
 
     @Override
     public String toString() {
-        return "From: "+getFrom()+"---"+getTo();
+	return "From: "+getFrom()+"---"+getTo();
     }
-    
-    
+
 }
--- src/cz/muni/stanse/xml2cfg/CFGNode.java
+++ src/cz/muni/stanse/xml2cfg/CFGNode.java
@@ -11,19 +11,17 @@ import java.util.Set;
 import java.util.HashSet;
 
 /**
- * Represents a node in control-flow graph (class ControlFlowGraph in this package)
+ * Represents a node in control-flow graph (class ControlFlowGraph in this
+ * package)
  */
-public class CFGNode {    
-    
+public class CFGNode {
+
     private static int numberCounter=0; // for generating an unique node number
-    
-    private int number; // node number, used in print 
-    
+    private int number; // node number, used in print
     private ControlFlowGraph cfg; // corresponding control-flow graph
-          
     private Set<CFGNode> predecessors = new HashSet<CFGNode>(); // represents predecessors of this node
     private Set<CFGNode> successors = new HashSet<CFGNode>(); // represents successors of this node
-    
+
     /**
      * Creates a new instance of CFGNode
      */
@@ -31,7 +29,7 @@ public class CFGNode {
         number = numberCounter++; // generating an unique node number
         this.cfg = cfg;
     }
-    
+
     /**
      * Get unique node number
      * @return unique node number
@@ -39,7 +37,7 @@ public class CFGNode {
     public int getNumber() {
         return number;
     }
-    
+
     /**
      * Get all predecessors of node
      * @return (read only) set of this node's predecessors
@@ -59,7 +57,7 @@ public class CFGNode {
         copy.addAll(successors);
         return copy;
     }
-    
+
     /**
      * Insert node to predecessors set
      * @param pred new predecesor
@@ -67,7 +65,7 @@ public class CFGNode {
     protected void addPred(CFGNode pred) {
         predecessors.add(pred);
     }
-    
+
     /**
      * Insert node to successors set
      * @param succ new successor
@@ -75,7 +73,7 @@ public class CFGNode {
     protected void addSucc(CFGNode succ) {
         successors.add(succ);
     }
-    
+
     /**
      * Get corresponding control-flow graph
      * @return control-flow graph
@@ -83,29 +81,29 @@ public class CFGNode {
     public ControlFlowGraph getCFG() {
         return cfg;
     }
-    
+
     /**
      * Join node with another node
      * @param temp node to join with
      */
     protected void joinWithNode(CFGNode temp) {
-        
+
         // correct all edges to "temp"
-        predecessors.addAll(temp.predecessors);        
+        predecessors.addAll(temp.predecessors);
         for (CFGNode tempPred : temp.predecessors) {
             if (tempPred.successors.remove(temp)) {
                 tempPred.successors.add(this);
-            }                                
+            }
         }
-        
+
         // correct all edges from "temp"
         successors.addAll(temp.successors);
         for (CFGNode tempSucc : temp.successors) {
             if (tempSucc.predecessors.remove(temp)) {
-                tempSucc.predecessors.add(this);                        
-            }        
+                tempSucc.predecessors.add(this);
+            }
         }
-   
+
     }
 
     @Override
@@ -139,12 +137,5 @@ public class CFGNode {
         return new Integer(number).toString();
     }
 
-   
-
-    
-    
-    
-    
-    
 }
 
