ea477b95e45fc5868f34b8f69ffe868ed4511252
xslaby <xslaby@98efdb2a-0913-0410-9d53-f03ad6a1d54d>
2009-08-03 11:48:57 +0000

    Stanse: whitespace cleanup
    s/        /\t/g
    
    I.e. tabs are 8 characters.
    
    git-svn-id: https://arran.fi.muni.cz/svn/stanse/trunk@699 98efdb2a-0913-0410-9d53-f03ad6a1d54d

--- src/cz/muni/stanse/Stanse.java
+++ src/cz/muni/stanse/Stanse.java
@@ -38,40 +38,40 @@ public final class Stanse {
     // public section
 
     public static void main(String[] args) {
-        printStanseInfo();
+	printStanseInfo();
 
-        final CmdLineManager cmdLineManager = new CmdLineManager(args);
-        if (cmdLineManager.infoMode()) {
-            cmdLineManager.printInfo(System.out);
-            return;
-        }
+	final CmdLineManager cmdLineManager = new CmdLineManager(args);
+	if (cmdLineManager.infoMode()) {
+	    cmdLineManager.printInfo(System.out);
+	    return;
+	}
 
-        getInstance().setVerbosityLevel(cmdLineManager.getVerbosityLevel());
+	getInstance().setVerbosityLevel(cmdLineManager.getVerbosityLevel());
 
-        buildConfiguration(cmdLineManager);
-        setOutputDirectory(cmdLineManager);
-        processDumping(cmdLineManager);
-        startUI(cmdLineManager);
+	buildConfiguration(cmdLineManager);
+	setOutputDirectory(cmdLineManager);
+	processDumping(cmdLineManager);
+	startUI(cmdLineManager);
     }
 
     public static Stanse getInstance() {
-        return stanse != null ? stanse : createSingleInstance();
+	return stanse != null ? stanse : createSingleInstance();
     }
 
     public Configuration getConfiguration() {
-        return configuration;
+	return configuration;
     }
 
     public synchronized void setConfiguration(Configuration configuration) {
-        this.configuration = configuration;
+	this.configuration = configuration;
     }
 
     public String getRootDirectory() {
-        return rootDirectory;
+	return rootDirectory;
     }
 
     public String getOutputDirectory() {
-        return outputDirectory;
+	return outputDirectory;
     }
 
     public static UnitManager getUnitManager() {
@@ -79,183 +79,183 @@ public final class Stanse {
     }
 
     public void setOutputDirectory(final String dir) {
-        outputDirectory = dir;
+	outputDirectory = dir;
     }
 
     public VerbosityLevel getVerbosityLevel() {
-        return verbosityLevel;
+	return verbosityLevel;
     }
 
     public void setVerbosityLevel(final VerbosityLevel level) {
-        verbosityLevel = level;
+	verbosityLevel = level;
     }
 
     public synchronized void dumpAST() {
-        assert(getConfiguration() != null);
-        for (final Unit unit: getConfiguration().getSourceConfiguration()
-                                                .getLazySourceInternals()
-                                                .getUnits()) {
-            final String name = getOutputDirectory().toString().isEmpty() ?
-            unit.getName() : (new File(unit.getName())).getName();
-            File xmlFile = new File(getOutputDirectory(), name + ".xml");
-            try {
-                XMLAlgo.outputXML(getUnitManager().getXMLDocument(unit),
-                new PrintStream(xmlFile));
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
+	assert(getConfiguration() != null);
+	for (final Unit unit: getConfiguration().getSourceConfiguration()
+						.getLazySourceInternals()
+						.getUnits()) {
+	    final String name = getOutputDirectory().toString().isEmpty() ?
+	    unit.getName() : (new File(unit.getName())).getName();
+	    File xmlFile = new File(getOutputDirectory(), name + ".xml");
+	    try {
+		XMLAlgo.outputXML(getUnitManager().getXMLDocument(unit),
+		new PrintStream(xmlFile));
+	    } catch (IOException exception) {
+		exception.printStackTrace();
+	    }
+	}
     }
 
     public synchronized void dumpCFG() {
-        assert(getConfiguration() != null);
-        for (final CFGHandle cfg: getConfiguration().getSourceConfiguration()
-                                                      .getLazySourceInternals()
-                                                      .getCFGHandles()) {
-            final String unitName = getUnitManager().getUnitName(cfg);
-            final String name = getOutputDirectory().toString().isEmpty() ?
-                                      unitName : (new File(unitName)).getName();
-            final File cfgFile = new File(getOutputDirectory(), name + "." +
-                                      cfg.getFunctionName() + ".dot");
-            try {
-                BufferedWriter out = new BufferedWriter(
-                                        new FileWriter(cfgFile));
-                out.write(cfg.toDot());
-                out.close();
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
+	assert(getConfiguration() != null);
+	for (final CFGHandle cfg: getConfiguration().getSourceConfiguration()
+						      .getLazySourceInternals()
+						      .getCFGHandles()) {
+	    final String unitName = getUnitManager().getUnitName(cfg);
+	    final String name = getOutputDirectory().toString().isEmpty() ?
+				      unitName : (new File(unitName)).getName();
+	    final File cfgFile = new File(getOutputDirectory(), name + "." +
+				      cfg.getFunctionName() + ".dot");
+	    try {
+		BufferedWriter out = new BufferedWriter(
+					new FileWriter(cfgFile));
+		out.write(cfg.toDot());
+		out.close();
+	    } catch (IOException exception) {
+		exception.printStackTrace();
+	    }
+	}
     }
 
     public synchronized void dumpCallGraph() {
-        assert(getConfiguration() != null);
-        final File file = new File(getOutputDirectory(),"call-graph.dot");
-        try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(file));
-            out.write(cz.muni.stanse.utils.CallGraphToDot.run(
-                                    getConfiguration().getSourceConfiguration()
-                                                      .getLazySourceInternals()
-                                                      .getCallGraph()));
-            out.close();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+	assert(getConfiguration() != null);
+	final File file = new File(getOutputDirectory(),"call-graph.dot");
+	try {
+	    BufferedWriter out = new BufferedWriter(new FileWriter(file));
+	    out.write(cz.muni.stanse.utils.CallGraphToDot.run(
+				    getConfiguration().getSourceConfiguration()
+						      .getLazySourceInternals()
+						      .getCallGraph()));
+	    out.close();
+	} catch (IOException exception) {
+	    exception.printStackTrace();
+	}
     }
 
     // private section
 
     private Stanse() {
-        configuration = null;
-        outputDirectory = ".";
-        rootDirectory = buildRootDirectory();
-        verbosityLevel = VerbosityLevel.LOW;
+	configuration = null;
+	outputDirectory = ".";
+	rootDirectory = buildRootDirectory();
+	verbosityLevel = VerbosityLevel.LOW;
     }
 
     private static synchronized Stanse createSingleInstance() {
-        return stanse != null ? stanse : (stanse = new Stanse());
+	return stanse != null ? stanse : (stanse = new Stanse());
     }
 
     private static void printStanseInfo() {
-        System.out.println("Stanse version \"0.9alpha\"");
-        System.out.println("Copyright (c) 2009 Masaryk University, Brno\n");
-        System.out.println("WARNING: This is an alpha-release. The " +
-                           "false-positive rate is not indicative of the " +
-                           "false-positive rate for the full release.\n");
+	System.out.println("Stanse version \"0.9alpha\"");
+	System.out.println("Copyright (c) 2009 Masaryk University, Brno\n");
+	System.out.println("WARNING: This is an alpha-release. The " +
+			   "false-positive rate is not indicative of the " +
+			   "false-positive rate for the full release.\n");
     }
 
     private static void buildConfiguration(final CmdLineManager cmdLineManager){
-        getInstance().setConfiguration(
-            new Configuration(cmdLineManager.getSourceConfiguration(),
-                              cmdLineManager.getCheckerConfiguration()));
+	getInstance().setConfiguration(
+	    new Configuration(cmdLineManager.getSourceConfiguration(),
+			      cmdLineManager.getCheckerConfiguration()));
     }
 
     private static void setOutputDirectory(final CmdLineManager cmdLineManager){
-        final String outDirName = cmdLineManager.getOutputDir();
-        if (outDirName == null)
-            return;
-        java.io.File outDir = new java.io.File(outDirName);
-        if (!outDir.exists()) {
-            System.out.print("Output directory: '" + outDirName +
-                             "' does not exist.\n\tCreating it...");
-            outDir.mkdir();
-            System.out.println(outDir.exists() ?
-                                    "Done.":"Failed! Output won't be written.");
-        }
-        getInstance().setOutputDirectory(outDirName);
+	final String outDirName = cmdLineManager.getOutputDir();
+	if (outDirName == null)
+	    return;
+	java.io.File outDir = new java.io.File(outDirName);
+	if (!outDir.exists()) {
+	    System.out.print("Output directory: '" + outDirName +
+			     "' does not exist.\n\tCreating it...");
+	    outDir.mkdir();
+	    System.out.println(outDir.exists() ?
+				    "Done.":"Failed! Output won't be written.");
+	}
+	getInstance().setOutputDirectory(outDirName);
     }
 
     private static void processDumping(final CmdLineManager cmdLineManager) {
-        if (getInstance().getConfiguration() == null) return;
-        if (cmdLineManager.dumpAST()) getInstance().dumpAST();
-        if (cmdLineManager.dumpCFG()) getInstance().dumpCFG();
-        if (cmdLineManager.dumpCallGraph()) getInstance().dumpCallGraph();
+	if (getInstance().getConfiguration() == null) return;
+	if (cmdLineManager.dumpAST()) getInstance().dumpAST();
+	if (cmdLineManager.dumpCFG()) getInstance().dumpCFG();
+	if (cmdLineManager.dumpCallGraph()) getInstance().dumpCallGraph();
     }
 
     private static void startUI(final CmdLineManager cmdLineManager) {
-        final Pair<String,String> UIdesc = cmdLineManager.getUIdesc();
-        if (UIdesc.getFirst().equals("GUI")) startGUI(UIdesc.getSecond());
-        else if (UIdesc.getFirst().equals("TUI")) startTUI(UIdesc.getSecond());
-        else { System.out.println("Warning: No user interface specified."); }
+	final Pair<String,String> UIdesc = cmdLineManager.getUIdesc();
+	if (UIdesc.getFirst().equals("GUI")) startGUI(UIdesc.getSecond());
+	else if (UIdesc.getFirst().equals("TUI")) startTUI(UIdesc.getSecond());
+	else { System.out.println("Warning: No user interface specified."); }
     }
 
     private static void startGUI(final String style) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                MainWindow.setLookAndFeel(style);
-                MainWindow.getInstance().setVisible(true);
-            }
-        });
+	java.awt.EventQueue.invokeLater(new Runnable() {
+	    public void run() {
+		MainWindow.setLookAndFeel(style);
+		MainWindow.getInstance().setVisible(true);
+	    }
+	});
     }
 
     private static void startTUI(final String style) {
-        // TODO: TUI is not properly implemented. Current implementation
-        //       simply executes checking procedure.
-        System.out.println("WARNING: TUI is not properly implemented. Current" +
-                           " implementation simply executes checking "+
-                           "procedure.\n");
-        System.out.println("Checking for bugs:");
-        final java.util.Vector<CheckerError> errors =
-                new java.util.Vector<CheckerError>();
-        getInstance().getConfiguration().evaluate_EachUnitSeparately(
-            new CheckerErrorReceiver() {
-                @Override
-                public void receive(final CheckerError error) {
-                    errors.add(error);
-                }
-                @Override
-                public void onEnd() {
-                    if (!errors.isEmpty()) {
-                        System.out.println("Error(s) found:");
-                        for (final CheckerError error : errors)
-                            System.out.println(error.toString());
-                    }
-                    System.out.println("Done.");
-                }
-            },
-            new CheckerProgressMonitor() {
-                @Override
-                public void write(final String s) {
-                    System.out.print(s);
-                }
-            });
+	// TODO: TUI is not properly implemented. Current implementation
+	//       simply executes checking procedure.
+	System.out.println("WARNING: TUI is not properly implemented. Current" +
+			   " implementation simply executes checking "+
+			   "procedure.\n");
+	System.out.println("Checking for bugs:");
+	final java.util.Vector<CheckerError> errors =
+		new java.util.Vector<CheckerError>();
+	getInstance().getConfiguration().evaluate_EachUnitSeparately(
+	    new CheckerErrorReceiver() {
+		@Override
+		public void receive(final CheckerError error) {
+		    errors.add(error);
+		}
+		@Override
+		public void onEnd() {
+		    if (!errors.isEmpty()) {
+			System.out.println("Error(s) found:");
+			for (final CheckerError error : errors)
+			    System.out.println(error.toString());
+		    }
+		    System.out.println("Done.");
+		}
+	    },
+	    new CheckerProgressMonitor() {
+		@Override
+		public void write(final String s) {
+		    System.out.print(s);
+		}
+	    });
     }
 
     private static String buildRootDirectory() {
-        String rootDirectory = System.getenv("STANSE_HOME");
-        if (rootDirectory == null) {
-            System.err.println("STANSE_HOME not specified. Using the " +
-                                "location of stanse.jar.");
-            try {
-                rootDirectory =
-                    new File(ClassLocation.get("cz.muni.stanse.Stanse"))
-                          .getParent();
-            } catch (final ClassNotFoundException e) {
-                e.printStackTrace();
-                rootDirectory = new File(".").getAbsolutePath();
-            }
-        }
-        return rootDirectory;
+	String rootDirectory = System.getenv("STANSE_HOME");
+	if (rootDirectory == null) {
+	    System.err.println("STANSE_HOME not specified. Using the " +
+				"location of stanse.jar.");
+	    try {
+		rootDirectory =
+		    new File(ClassLocation.get("cz.muni.stanse.Stanse"))
+			  .getParent();
+	    } catch (final ClassNotFoundException e) {
+		e.printStackTrace();
+		rootDirectory = new File(".").getAbsolutePath();
+	    }
+	}
+	return rootDirectory;
     }
 
     private static UnitManager unitManager = new UnitManagerLRU();
