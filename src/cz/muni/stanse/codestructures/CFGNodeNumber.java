eb2b8db041a9b911c8c667c9cd49cabfa724d8d8
xslaby <xslaby@98efdb2a-0913-0410-9d53-f03ad6a1d54d>
2009-08-03 11:47:24 +0000

    codestructures: make CFGNodeNumber synchronized
    git-svn-id: https://arran.fi.muni.cz/svn/stanse/trunk@697 98efdb2a-0913-0410-9d53-f03ad6a1d54d

--- src/cz/muni/stanse/codestructures/CFGNodeNumber.java
+++ src/cz/muni/stanse/codestructures/CFGNodeNumber.java
@@ -3,7 +3,7 @@ package cz.muni.stanse.codestructures;
 public class CFGNodeNumber {
     private static int number;
 
-    public static int getNext() {
+    public synchronized static int getNext() {
 	return number++;
     }
 }
