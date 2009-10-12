58d1c0464b05bf282f6faa85f6467f5294e5874b
xslaby <xslaby@98efdb2a-0913-0410-9d53-f03ad6a1d54d>
2009-10-12 16:05:10 +0000

    configuration: fix Makefile Enumerator
    It was a little bit broken. Almost a rewrite :).
    
    git-svn-id: https://arran.fi.muni.cz/svn/stanse/trunk@838 98efdb2a-0913-0410-9d53-f03ad6a1d54d

--- src/cz/muni/stanse/configuration/source_enumeration/MakefileSourceEnumerator.java
+++ src/cz/muni/stanse/configuration/source_enumeration/MakefileSourceEnumerator.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 public final class MakefileSourceEnumerator extends
-                                           ReferencedSourceCodeFileEnumerator {
+	ReferencedSourceCodeFileEnumerator {
     // public section
 
     public MakefileSourceEnumerator(final String makeFile,
@@ -20,16 +20,18 @@ public final class MakefileSourceEnumerator extends
     }
 
     @Override
-    public List<String> getSourceCodeFiles() throws SourceCodeFilesException {
-        List<String> result;
-        synchronized(getClass()) {
-            final String batchFile = createBatchFile(getReferenceFile(),
-                                                     getArguments());
-            result = new BatchFileEnumerator(batchFile).getSourceCodeFiles();
-            if (!new File(batchFile).delete())
-		    System.err.println("Can't delete " + batchFile);
-        }
-        return result;
+    public synchronized List<String> getSourceCodeFiles()
+	    throws SourceCodeFilesException {
+	if (files == null) {
+	    final File batchFile = createBatchFile(getReferenceFile(),
+						 getArguments());
+	    files = new BatchFileEnumerator(batchFile.getAbsolutePath()).
+		    getSourceCodeFiles();
+          if (!batchFile.delete())
+		System.err.println("Can't delete " +
+			batchFile.getAbsolutePath());
+	}
+	return files;
     }
 
     public String getArguments() {
@@ -38,20 +40,50 @@ public final class MakefileSourceEnumerator extends
 
     // private section
 
-    private static String createBatchFile(final String makeFile,
+    private static File createBatchFile(final String makeFile,
                                           final String arguments)
                                           throws SourceCodeFilesException {
-        final String batchFile = "/tmp/tmp_stanse_batch_file_for_makefile.txt";
-        final ProcessBuilder builder = new ProcessBuilder(createMakeCmdLine(
-                                                                    arguments));
+	final File makefile = new File(makeFile);
+	if (!makefile.exists())
+	    throw new SourceCodeFilesException("Makefile '" +
+		    makefile.getAbsolutePath() + "' doesn't exist");
+        final File batchFile;
+	try {
+	    batchFile = File.createTempFile("stanse_jobfile", null);
+	} catch (IOException e) {
+	    System.err.println("Can't create a temp jobfile");
+	    throw new SourceCodeFilesException(e);
+	}
+	ProcessBuilder builder = new ProcessBuilder(createMakeCmdLine(
+		makefile, arguments));
         final Map<String, String> environment = builder.environment();
-        environment.put("JOB_FILE",batchFile);
+        environment.put("JOB_FILE", batchFile.getAbsolutePath());
         environment.put("PATH", environment.get("PATH") + File.pathSeparator +
                         Stanse.getInstance().getRootDirectory() +
                         File.separator + "bin");
 	try {
-	    builder.directory(new File(makeFile).getParentFile()).start().
-		waitFor();
+	    File dir = makefile.getParentFile();
+	    if (dir == null)
+		dir = new File(".");
+	    builder.directory(dir);
+	    environment.put("PWD", dir.getAbsolutePath());
+	    Process p = builder.start();
+	    p.waitFor();
+	    /*char b[] = new char[1600];
+	    InputStreamReader isr = new InputStreamReader(p.getErrorStream());
+	    OutputStreamWriter osw = new OutputStreamWriter(System.err);
+	    int len = isr.read(b);
+	    System.err.println("======err:");
+	    if (len > 0)
+		osw.write(b, 0, len);
+	    osw.flush();
+	    System.err.println("======out:");
+	    isr = new InputStreamReader(p.getInputStream());
+	    len = isr.read(b);
+	    if (len > 0)
+		osw.write(b, 0, len);
+	    osw.flush();
+	    System.err.println("======");*/
 	} catch (IOException e) {
 	    throw new SourceCodeFilesException(e);
 	} catch (InterruptedException e) {
@@ -60,12 +92,15 @@ public final class MakefileSourceEnumerator extends
         return batchFile;
     }
 
-    private static List<String> createMakeCmdLine(final String args) {
-        final List<String> result = Make.<String>linkedList("make" ,"CC=stcc");
+    private static List<String> createMakeCmdLine(final File makefile,
+	    final String args) {
+        final List<String> result = Make.<String>linkedList("make", "CC=stcc",
+		"-f" + makefile.getName());
         if (!args.isEmpty())
             result.addAll(java.util.Arrays.asList(args.split("[ \t]")));
         return result;
     }
 
     private final String arguments;
+    private List<String> files = null;
 }
