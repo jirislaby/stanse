f2ac0bd0d0b7bd9510cf5f5507e5c6b08f2d69fe
xslaby <xslaby@98efdb2a-0913-0410-9d53-f03ad6a1d54d>
2010-02-03 10:16:57 +0000

    bump to 1.0.8
    git-svn-id: https://arran.fi.muni.cz/svn/stanse/trunk@947 98efdb2a-0913-0410-9d53-f03ad6a1d54d

--- src/cz/muni/stanse/Stanse.java
+++ src/cz/muni/stanse/Stanse.java
@@ -175,8 +175,8 @@ public final class Stanse {
     }
 
     private static void printStanseInfo() {
-	System.out.println("Stanse version \"1.0.7\"");
-	System.out.println("Copyright (c) 2008-2009 Masaryk University, Brno\n");
+	System.out.println("Stanse version \"1.0.8\"");
+	System.out.println("Copyright (c) 2008-2010 Masaryk University, Brno\n");
     }
 
     private static void buildConfiguration(final CmdLineManager cmdLineManager){
