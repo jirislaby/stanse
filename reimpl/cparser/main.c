0b5e15288287467ace591fe52fc74493f12af503
xslaby <xslaby@98efdb2a-0913-0410-9d53-f03ad6a1d54d>
2010-11-17 22:28:01 +0000

    reimpl: add cparser
    It is relatively easy to implement a C parser in C that generates AST
    (and then CFG).
    
    git-svn-id: https://arran.fi.muni.cz/svn/stanse/trunk@1158 98efdb2a-0913-0410-9d53-f03ad6a1d54d

---
+++ reimpl/cparser/main.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+#include <antlr3defs.h>
+
+#include "GNUCaLexer.h"
+#include "GNUCaParser.h"
+
+int main(int argc, char *argv[])
+{
+	pANTLR3_INPUT_STREAM input;
+	pANTLR3_COMMON_TOKEN_STREAM tstream;
+	GNUCaParser_translationUnit_return parseTree;
+	pGNUCaLexer lxr;
+	pGNUCaParser psr;
+	int ret = 1;
+
+	if (argc != 2)
+		goto err;
+
+	ret++;
+	input = antlr3AsciiFileStreamNew((unsigned char *)argv[1]);
+	if (!input)
+		goto err;
+	ret++;
+	lxr = GNUCaLexerNew(input);
+	if (!lxr)
+		goto err_input;
+	ret++;
+	tstream = antlr3CommonTokenStreamSourceNew(ANTLR3_SIZE_HINT, TOKENSOURCE(lxr));
+	if (!tstream)
+		goto err_lxr;
+	ret++;
+	psr = GNUCaParserNew(tstream);
+	if (!psr)
+		goto err_tstream;
+
+	parseTree = psr->translationUnit(psr);
+
+	puts((char *)parseTree.tree->toStringTree(parseTree.tree)->chars);
+
+	ret = 0;
+
+	psr->free(psr);
+err_tstream:
+	tstream->free(tstream);
+err_lxr:
+	lxr->free(lxr);
+err_input:
+	input->close(input);
+err:
+	return ret;
+}
