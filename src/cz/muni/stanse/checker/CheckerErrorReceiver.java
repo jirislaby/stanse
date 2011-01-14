d43c4cdf0c61d2e4007e3d48bab1e9a8cfceb6a5
Jiri Slaby <jirislaby@gmail.com>
2011-01-14 11:53:20 +0100

    CheckerErrorReceiver: add support for addAll

--- src/cz/muni/stanse/checker/CheckerErrorReceiver.java
+++ src/cz/muni/stanse/checker/CheckerErrorReceiver.java
@@ -1,13 +1,17 @@
 package cz.muni.stanse.checker;
 
+import java.util.Collection;
+
 public class CheckerErrorReceiver {
 
-    // public section
+	public void receiveAll(final Collection<CheckerError> errors) {
+		for (final CheckerError error : errors)
+			receive(error);
+	}
 
-    public void receive(final CheckerError error) {
-    }
+	public void receive(final CheckerError error) {
+	}
 
-    public void onEnd() {
-    }
-
+	public void onEnd() {
+	}
 }
