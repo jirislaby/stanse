931975d4c4bab09e0fd12616eba73b6a7c525be0
Jiri Slaby <jirislaby@gmail.com>
2011-01-18 16:50:40 +0100

    lockchecker: LockChecker, refactor check method
    Eliminate code duplication.

--- src/cz/muni/stanse/lockchecker/LockChecker.java
+++ src/cz/muni/stanse/lockchecker/LockChecker.java
@@ -1,5 +1,6 @@
 package cz.muni.stanse.lockchecker;
 
+import java.util.Collection;
 import org.apache.log4j.Logger;
 
 import cz.muni.stanse.checker.Checker;
@@ -19,12 +20,9 @@ import cz.muni.stanse.codestructures.LazyInternalStructures;
  *
  */
 public class LockChecker extends Checker {
-
-    private Configuration conf;
-
-    private final static Logger logger =
-	Logger.getLogger(SummariesBuilder.class.getName());
-
+	private Configuration conf;
+	private final static Logger logger =
+		Logger.getLogger(LockChecker.class.getName());
 
 	/**
 	 * Constructs the Lock Checker using given configuration
@@ -35,41 +33,47 @@ public class LockChecker extends Checker {
 	}
 
 	@Override
-    public CheckingResult check(final LazyInternalStructures internals,
+	public CheckingResult check(final LazyInternalStructures internals,
 		final CheckerErrorReceiver errReceiver,
 		final CheckerProgressMonitor monitor) throws CheckerException {
-	final SummariesBuilder summariesBuilder = new SummariesBuilder(
-		internals.getNodeToCFGdictionary(),
-		internals.getArgumentPassingManager(), conf);
+		final SummariesBuilder summariesBuilder = new SummariesBuilder(
+			internals.getNodeToCFGdictionary(),
+			internals.getArgumentPassingManager(), conf);
 
-	for(CFGHandle handle : internals.getStartFunctions()) {
-		summariesBuilder.traverse(handle.getStartNode(),
-			internals.getNavigator(), new State());
+		for (CFGHandle handle : internals.getStartFunctions()) {
+			summariesBuilder.traverse(handle.getStartNode(),
+				internals.getNavigator(), new State());
+		}
+
+		final Summaries sum = summariesBuilder.getSummaries();
+		final CheckerErrorFilter filter = new CheckerErrorFilter();
+
+		if (conf.onlyTopFunctions()) {
+			for (final CFGHandle cfgh :
+					internals.getStartFunctions()) {
+				final FunctionSummary fs =
+					sum.get(cfgh.getStartNode());
+				checkFunctions(fs.getFunctionStateSummaries(),
+					errReceiver, filter);
+			}
+		} else {
+			checkFunctions(sum.getAllFunctionStateSummaries(),
+				errReceiver, filter);
+		}
+
+		filter.generateErrors(errReceiver);
+
+		logger.debug(sum);
+
+		return new CheckingSuccess();
 	}
 
-	final Summaries sum = summariesBuilder.getSummaries();
-	final CheckerErrorFilter filter = new CheckerErrorFilter();
-
-	if (conf.onlyTopFunctions()) {
-		for (final CFGHandle handle : internals.getStartFunctions()) {
-			final FunctionSummary functionSummary =
-				sum.get(handle.getStartNode());
-			for (final FunctionStateSummary fss :
-				functionSummary.getFunctionStateSummaries()) {
-				final ErrorGenerator generator =
-					new ErrorGenerator(fss, conf.countFlows(),
-					conf.countPairs(), conf.getThreshold(),
-					conf.generateMoreLocksErrors());
-				generator.generateErrors(filter);
-				if (conf.generateDoubleErrors())
-					fss.getErrHolder().save(errReceiver);
-			}
-		}
-	} else {
-		for (final FunctionStateSummary fss :
-				sum.getAllFunctionStateSummaries()) {
-			final ErrorGenerator generator = new ErrorGenerator(
-				fss, conf.countFlows(), conf.countPairs(),
+	private void checkFunctions(final Collection<FunctionStateSummary> sums,
+			final CheckerErrorReceiver errReceiver,
+			final CheckerErrorFilter filter) {
+		for (final FunctionStateSummary fss : sums) {
+			final ErrorGenerator generator = new ErrorGenerator(fss,
+				conf.countFlows(), conf.countPairs(),
 				conf.getThreshold(),
 				conf.generateMoreLocksErrors());
 			generator.generateErrors(filter);
@@ -78,16 +82,9 @@ public class LockChecker extends Checker {
 		}
 	}
 
-	filter.generateErrors(errReceiver);
-
-	logger.debug(sum);
-
-	return new CheckingSuccess();
-    }
-
-    @Override
-    public String getName() {
-	return "Lock Checker for finding variables which should be locked " +
-		"but are not";
-    }
+	@Override
+	public String getName() {
+		return "Lock Checker for finding variables which should be " +
+			" locked but are not";
+	}
 }
