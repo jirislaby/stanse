c1f6d7f0b50e306e115985e07d5c4cdc80460d8f
Jiri Slaby <jirislaby@gmail.com>
2011-02-19 15:32:35 +0100

    reimpl: cparser, handle ops in {Post,Pre}fixExpression
    Define a conversion table, parse etc...

--- reimpl/cparser/cparser/AST/PostfixExpression.java
+++ reimpl/cparser/cparser/AST/PostfixExpression.java
@@ -4,18 +4,32 @@
 
 package cparser.AST;
 
+import cparser.Table;
+
 /**
  * @author Jiri Slaby
  */
 public class PostfixExpression extends Node {
-	final String op;
+	public enum POSTFIX_EXPR {
+		PLUSPLUS,
+		MINUSMINUS,
+		PE_SIZE
+	}
+	private static final Table<POSTFIX_EXPR> ops;
+	private final POSTFIX_EXPR op;
+
+	static {
+		ops = new Table<POSTFIX_EXPR>(POSTFIX_EXPR.PE_SIZE.ordinal());
+		ops.fill("++", POSTFIX_EXPR.PLUSPLUS);
+		ops.fill("--", POSTFIX_EXPR.MINUSMINUS);
+	}
 
 	public PostfixExpression(final String op) {
-		this.op = op;
+		this.op = ops.getVal(op);
 	}
 
 	@Override
 	void XMLAttributes(final StringBuilder sb) {
-		sb.append(" op=\"").append(op).append('"');
+		sb.append(" op=\"").append(ops.getStr(op)).append('"');
 	}
 }
--- reimpl/cparser/cparser/AST/PrefixExpression.java
+++ reimpl/cparser/cparser/AST/PrefixExpression.java
@@ -4,19 +4,48 @@
 
 package cparser.AST;
 
+import cparser.Table;
+
 /**
  * @author Jiri Slaby
  */
 public class PrefixExpression extends Node {
-	final String op;
+	public enum PREFIX_EXPR {
+		PLUS,
+		MINUS,
+		PLUSPLUS,
+		MINUSMINUS,
+		NEG,
+		NOT,
+		LABREF,
+		REAL,
+		IMAG,
+		PE_SIZE
+	}
+	private static final Table<PREFIX_EXPR> ops;
+	private final PREFIX_EXPR op;
+
+	static {
+		ops = new Table<PREFIX_EXPR>(PREFIX_EXPR.PE_SIZE.ordinal());
+		ops.fill("+", PREFIX_EXPR.PLUS);
+		ops.fill("-", PREFIX_EXPR.MINUS);
+		ops.fill("++", PREFIX_EXPR.PLUSPLUS);
+		ops.fill("--", PREFIX_EXPR.MINUSMINUS);
+		ops.fill("~", PREFIX_EXPR.NEG);
+		ops.fill("!", PREFIX_EXPR.NOT);
+		ops.fill("&&", PREFIX_EXPR.LABREF);
+		ops.fill("__real", PREFIX_EXPR.REAL);
+		ops.fill("__imag", PREFIX_EXPR.IMAG);
+	}
 
 	public PrefixExpression(final String op) {
-		this.op = op;
+		this.op = ops.getVal(op);
 	}
 
 	@Override
 	void XMLAttributes(final StringBuilder sb) {
-		sb.append(" op=\"").append(BinaryExpression.encodeXML(op)).
+		sb.append(" op=\"").
+			append(BinaryExpression.encodeXML(ops.getStr(op))).
 			append('"');
 	}
 }
