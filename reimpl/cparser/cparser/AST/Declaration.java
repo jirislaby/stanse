94086c083d7523895f40028453ceca5f02bbbf84
Jiri Slaby <jirislaby@gmail.com>
2011-02-24 21:53:16 +0100

    reimpl: cparser, create CFG for Declaration
    It's only Init Declaration which matters here.

--- reimpl/cparser/cparser/AST/Declaration.java
+++ reimpl/cparser/cparser/AST/Declaration.java
@@ -4,21 +4,38 @@
 
 package cparser.AST;
 
+import cparser.CFG.CFG;
+import cparser.CFG.CFGFiller;
+import cparser.CFG.CFGNode;
+
 /**
  * @author Jiri Slaby
  */
-public class Declaration extends Node {
+public class Declaration extends Node implements CFGFiller {
+	private boolean isTypedef = false;
 
 	@Override
 	public void compute() {
 		super.compute();
-/*		System.out.println("decl: " + toXML());
 		final DeclarationSpecifiers ds =
 			(DeclarationSpecifiers)getChild(0);
-		boolean tdef = false;
 		if (ds.getModifiers().hasSC(Modifiers.SC.TYPEDEF))
-			tdef = true;
-		System.out.println("tdef=" + tdef);*/
+			isTypedef = true;
+		if (isTypedef)
+			return;
+	}
+
+	public void fillCFG(CFG cfg) {
+		int a;
+
+		if (isTypedef)
+			return;
+
+		for (a = 1; a < children.size(); a++) {
+			final InitDeclarator id = (InitDeclarator)getChild(a);
+			if (id.hasInitializer())
+				cfg.append(new CFGNode(id));
+		}
 	}
 
 }
