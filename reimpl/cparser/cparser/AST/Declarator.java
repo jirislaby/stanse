d552b4cbf2713e821b0300323ca23034dab9a28f
Jiri Slaby <jirislaby@gmail.com>
2011-02-24 18:30:42 +0100

    reimpl: cparser, compute declarator ID

--- reimpl/cparser/cparser/AST/Declarator.java
+++ reimpl/cparser/cparser/AST/Declarator.java
@@ -8,5 +8,21 @@ package cparser.AST;
  * @author Jiri Slaby
  */
 public class Declarator extends Node {
+	private Id id;
 
+	@Override
+	public void compute() {
+		super.compute();
+		Node child = getChild(0);
+		if (child instanceof Pointer)
+			child = getChild(1);
+		if (child instanceof Declarator)
+			id = ((Declarator)child).getId();
+		else
+			id = (Id)child;
+	}
+
+	public Id getId() {
+		return id;
+	}
 }
