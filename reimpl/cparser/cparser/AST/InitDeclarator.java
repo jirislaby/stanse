2a729dd0f688467e0c494bb2fa1cba5d7b444293
Jiri Slaby <jirislaby@gmail.com>
2011-02-24 20:36:34 +0100

    reimpl: cparser, compute InitDeclarator

--- reimpl/cparser/cparser/AST/InitDeclarator.java
+++ reimpl/cparser/cparser/AST/InitDeclarator.java
@@ -8,5 +8,24 @@ package cparser.AST;
  * @author Jiri Slaby
  */
 public class InitDeclarator extends Node {
+	private boolean hasInitializer = false;
 
+	@Override
+	public void compute() {
+		super.compute();
+		if (children.size() > 1)
+			hasInitializer = true;
+	}
+
+	public Declarator getDeclarator() {
+		return (Declarator)getChild(0);
+	}
+
+	public boolean hasInitializer() {
+		return hasInitializer;
+	}
+
+	public Initializer getInitializer() {
+		return hasInitializer ? (Initializer)getChild(1) : null;
+	}
 }
